37af19b70d35ccac220b7158adb04e9dbf37f848
joshsack1 <80927517+joshsack1@users.noreply.github.com>
2024-01-16 13:52:14 -0500

    added visual surround plugin

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -9,4 +9,17 @@ return {
         "kawre/neotab.nvim",
         event = "InsertEnter",
     },
+    {
+        "NStefan002/visual-surround.nvim",
+        config = function()
+            require("visual-surround").setup({
+                -- if set to true, the user must manually add keymaps
+                use_default_keymaps = true,
+                -- will be ignored if use_default_keymaps is set to false
+                surround_chars = { "{", "}", "[", "]", "(", ")", "'", '"', "`" },
+                -- whether to exit visual mode after adding surround
+                exit_visual_mode = true,
+            })
+        end,
+    }
 }
