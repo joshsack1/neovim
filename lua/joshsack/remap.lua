026c1ae06d64a880fbfeae04fb60edad337afa17
joshsack1 <80927517+joshsack1@users.noreply.github.com>
2024-01-19 13:49:46 -0500

    added new remaps that should make insert mode more effective

--- lua/joshsack/remap.lua
+++ lua/joshsack/remap.lua
@@ -1,3 +1,10 @@
 -- Easily move lines around
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Make it so that in visual mode it is easy to move to the beginning and end of each line 
+vim.keymap.set("i", "<C-h>", "<C-o>I")
+vim.keymap.set("i", "<C-l>", "<C-o>A")
+-- Make it so that in insert mode, it is easy to select to the begging, end, or whole line in visual mode
+vim.keymap.set("i", "<C-a>", "<Esc>^V")
+vim.keymap.set("i", "<C-S-h>", "<Esc>v_")
+vim.keymap.set("i", "<C-S-l>", "<Esc>v$")
