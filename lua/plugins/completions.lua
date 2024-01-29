9f693f4199464cdca1e84b65ba2fefa718b73e4c
joshsack1 <80927517+joshsack1@users.noreply.github.com>
2024-01-29 14:15:37 -0500

    added noice and cmd-cmp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -3,6 +3,9 @@ return {
         "hrsh7th/cmp-nvim-lsp"
     },
     {
+        "hrsh7th/cmp-cmdline"
+    },
+    {
         "dcampos/cmp-snippy",
     },
     {
@@ -49,6 +52,27 @@ return {
                 }, {
                         { name = "buffer" },
                     }),
+            })
+            -- `/` cmdline setup.
+            cmp.setup.cmdline('/', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+            -- `:` cmdline setup.
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                        {
+                            name = 'cmdline',
+                            option = {
+                                ignore_cmds = { 'Man', '!' }
+                            }
+                        }
+                    })
             })
         end,
     },
