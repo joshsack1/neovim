aaade50e18f59be9cb9db7ea4f982d21c5a80815
joshsack1 <80927517+joshsack1@users.noreply.github.com>
2024-02-06 08:29:30 -0500

    set filetype plugin for markdown

--- after.bak/luasnip.lua
+++
@@ -1,8 +0,0 @@
-local ls = require"luasnip"
-ls.config.set_config {
-    vim.keymap.set({"i"}, "<Tab>", function() ls.expand() end, {silent = true}),
-    vim.keymap.set({"i", "s"}, "<Tab>", function() ls.jump( 1) end, {silent = true}),
-    vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump(-1) end, {silent = true}),
-}
-
-require("luasnip.loaders.from_lua").lazy_load({paths = "~/.config/nvim/LuaSnip/"})
