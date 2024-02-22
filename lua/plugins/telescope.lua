8b58c395deb33593d9397b60d566902fbd6c1118
joshsack1 <80927517+joshsack1@users.noreply.github.com>
2024-02-22 17:52:20 -0500

    potentially replaced obsidian with telekasten

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -48,5 +48,55 @@ return {
             -- load_extension, somewhere after setup function:
             require("telescope").load_extension("ui-select")
         end,
-    }
+    },
+    {
+        "renerocksai/telekasten.nvim",
+        config = function ()
+            home = vim.fn.expand("/Users/joshsack/Library/Mobile Documents/iCloud~md~obsidian/Documents/MainNotes/PARA")
+            require("telekasten").setup({
+                -- markdown file extension
+                extension    = ".md",
+
+                -- following a link to a non-existing note will create it
+                follow_creates_nonexisting = true,
+                dailies_create_nonexisting = true,
+                weeklies_create_nonexisting = true,
+                -- image link style
+                -- wiki:     ![[image name]]
+                -- markdown: ![](image_subdir/xxxxx.png)
+                image_link_style = "wiki",
+                 -- telescope actions behavior
+                close_after_yanking = false,
+                insert_after_inserting = true,
+
+                -- tag notation: '#tag', ':tag:', 'yaml-bare'
+                tag_notation = "#tag",
+
+                -- command palette theme: dropdown (window) or ivy (bottom panel)
+                command_palette_theme = "dropdown",
+
+                -- tag list theme:
+                -- get_cursor: small tag list at cursor; ivy and dropdown like above
+                show_tags_theme = "dropdown",
+
+                -- when linking to a note in subdir/, create a [[subdir/title]] link
+                -- instead of a [[title only]] link
+                subdirs_in_links = true,
+                new_note_location = "smart",
+
+                -- should all links be updated when a file is renamed
+                rename_update_links = true,
+                -- On hesitation, bring up the command panel
+                vim.keymap.set('n', '<leader>pp', ':lua require("telekasten").panel()<CR>'),
+                -- set \nn to create a new note with :lua require("telekasten").new_note()<CR>
+                vim.keymap.set('n', '<leader>nn', ':lua require("telekasten").new_note()<CR>'),
+                -- Set \rn to rename a note with :lua require("telekasten").rename_note()<CR>
+                vim.keymap.set('n', '<leader>rn', ':lua require("telekasten").rename_note()<CR>'),
+                -- Set \zz to toggle todo with :lua require("telekasten").toggle_todo()<CR>
+                vim.keymap.set('n', '<leader>zz', ':lua require("telekasten").toggle_todo()<CR>'),
+                -- Map S-<CR> to follow a link with :lua require("telekasten").follow_link()<CR>
+                vim.keymap.set('n', '<S-CR>', ':lua require("telekasten").follow_link()<CR>'),
+            })
+        end
+    },
 }
